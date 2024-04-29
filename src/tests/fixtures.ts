22d17abf3dde1a54c77ff2acc0f69fdaa7c653a0
zain5555 <zainabedeen.sohail@gmail.com>
2024-04-29 13:12:53 +1000

    Intial.

--- src/tests/fixtures.ts
+++
@@ -1,9 +0,0 @@
-// import { RatingDto } from '../ratings/http/ratings.controller';
-
-// export function ratingDto(overrides?: Partial<RatingDto>): RatingDto {
-//   return {
-//     coffeeType: 'any coffee type',
-//     starRating: '1/5',
-//     ...overrides,
-//   };
-// }
